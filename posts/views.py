feb12e6de95295d50468ebb131030034096b2fa2
Karachalovvv <karachalov@gmail.com>
2025-07-25 04:15:25 +0600

    HW3

--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, HttpResponse
 from posts.models import Post
 
 def home(request):
-    return render(request, "home.html")
-
-def html_view(request):
     return render(request, "base.html")
-
 
 def post_list_view(request):
     posts = Post.objects.all()
     return render(request, "posts/post_list.html", context = {"posts": posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.filter(id = post_id).first()
+    return render(request, "posts/post_detail.html", context = {"post": post})
