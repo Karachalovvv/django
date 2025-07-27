fe31705f79d718b47103fb6f252966f42a165a17
Karachalovvv <karachalov@gmail.com>
2025-07-28 00:10:37 +0600

    HW5

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from posts.models import Post
 from posts.forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     if request.method == "GET":
@@ -8,11 +9,13 @@ def home(request):
     else:
         return HttpResponse("Hello, world. You're at thepolis index")
 
+@login_required(login_url = "/login")
 def post_list_view(request):
     if request.method == "GET":
         posts = Post.objects.all()
         return render(request, "posts/post_list.html", context = {"posts": posts})
 
+@login_required(login_url = "/login")
 def post_detail_view(request, post_id):
     if request.method == "GET":
         post = Post.objects.filter(id = post_id).first()
@@ -20,9 +23,11 @@ def post_detail_view(request, post_id):
     
 
 def post_create_view(request):
+    
     if request.method == "GET":
         form = PostForm()
         return render(request, "posts/post_create.html", context = {"form": form})
+    
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if not form.is_valid():
